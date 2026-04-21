b8505d51a587e781bfa7cbdd6eb922e759bd8583
widiansakhara <wdiansakhara@gmail.com>
2026-04-21 13:04:13 +0700

    first commit

---
+++ test/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<body>
+<html lang="en">
+    <body>
+<div id="content">
+    <table border="1" width="10%">
+    <tr>
+        <td width="10%">
+            <img src="https://1.bp.blogspot.com/-yAQgSUZf2oY/VRIVSGc8rvI/AAAAAAAABA0/DkDFFf71muY/s1600/Logo+UNSIQ+Polos.jpg" alt="unsiq", width="100", height="100">
+</td>
+<td width="90%">
+    <h2>Selamat Datang di Mata Kuliah pemrograman web</h2>
+    <h3>Pertemuan 2: HTML Dasar</h3>
+</td>
+</tr>
+</table>
+<p align="center">Mata kuliah ini memperkenalkan dasar dasar pemrograman web menggunakan HTML,CSS,dan javaScript pada pertemuan ini, kita fokus pada struktur dan elemen dasar HTML
+    <hr>
+    HTML adalah bahasa markup yang digunakan untuk membuat struktur halaman web. Dengan HTML, kita bisa menampilkan teks,gambar,dan link secara terorganisir. untuk informasi lengkap dari HTML dapat dikunjungi link berikut:
+        <a href="https://www.hostinger.com/id/tutorial/apa-itu-html">penjelasan HTML</a>
+        <h3>Topik Utama pertemuan ini:</h3>
+        <ul>
+            <li>Struktur HTML</li>
+            <li>Heading dan paragraf</li>
+            <li>List(Unordered dan ordered)</li>
+            <li>Link dan Gambar</li>
+            <li>Tabel</li>
+
+            <h3>Langkah Praktikum:</h3>
+            <ol>
+                <li>Buat file index.php</li>
+                <li>Tulis kode HTML</li>
+                <li>Simpan dan buka di browser</li>
+                <ul>
+                    <li>buka xampp</li>
+                    <li>nyalain server</li>
+                    <li>buka browser dengan uri localhost</li>
+    </li>
+    </ul>
+<li>verivikasi hasil</li>
+<ol>
+</html>
